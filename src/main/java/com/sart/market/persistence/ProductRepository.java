ea3ef3e1af76b9c0775a777cff7da3453dcffdc5
Sergio Rojas <sart1991@gmail.com>
2021-04-14 16:48:58 -0500

    crud product repository

--- src/main/java/com/sart/market/persistence/ProductRepository.java
+++ src/main/java/com/sart/market/persistence/ProductRepository.java
@@ -3,15 +3,21 @@ package com.sart.market.persistence;
 
 import com.sart.market.persistence.crud.ProductCrud;
 import com.sart.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+@Repository
 public class ProductRepository {
 
     private ProductCrud crud;
+
+    public Optional<Product> selectById(Long id) {
+        return crud.findById(id);
+    }
 
     public List<Product> selectAll() {
         return StreamSupport.stream(crud.findAll().spliterator(), false)
@@ -24,6 +30,14 @@ public class ProductRepository {
 
     public Optional<List<Product>> selectByStockLessThan(int stock) {
         return crud.findByStockLessThanAndState(stock, true);
+    }
+
+    public Product insert(Product product) {
+        return crud.save(product);
+    }
+
+    public void deleteById(Long id) {
+        crud.deleteById(id);
     }
 
 }
