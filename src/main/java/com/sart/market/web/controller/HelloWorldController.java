1e8450c4a786ab0064a0a594edf7112982627b8d
Sergio Rojas <sart1991@gmail.com>
2021-04-14 09:49:19 -0500

    initial project structure

---
+++ src/main/java/com/sart/market/web/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.sart.market.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Never stop learning!!";
+    }
+
+}
