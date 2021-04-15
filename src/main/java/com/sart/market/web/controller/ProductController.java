89d1f28a51abf086a91bc12d9872b5ffd5979b88
Sergio Rojas <sart1991@gmail.com>
2021-04-14 23:48:06 -0500

    product controller working

--- src/main/java/com/sart/market/web/controller/ProductController.java
+++ src/main/java/com/sart/market/web/controller/ProductController.java
@@ -1,4 +1,54 @@
 package com.sart.market.web.controller;
 
+import com.sart.market.domain.Product;
+import com.sart.market.domain.service.ProductService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") long id) {
+        return productService.getProductById(id)
+                .map(p -> new ResponseEntity<>(p, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return productService.getAllProducts()
+                .map(lp -> new ResponseEntity<>(lp, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public List<Product> getAllProductsByCategory(@PathVariable("categoryId") long categoryId) {
+        return productService.getAllProductsByCategory(categoryId).orElseThrow();
+    }
+
+    @GetMapping(params = "maxStock")
+    public List<Product> getAllProductsLessThanStock(int maxStock) {
+        return productService.getAllProductsLessThanStock(maxStock).orElseThrow();
+    }
+
+    @PostMapping()
+    public Product saveProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteProduct(@PathVariable("id") long id) {
+        return productService.deleteProduct(id);
+    }
+
 }
